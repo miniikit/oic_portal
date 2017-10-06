fe5596009c0712e227e1dfa2fc8c2a7d1467a521
fd3s7 <B5021@oic.jp>
2017-10-06 12:06:59 +0900

    カードの修正

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,25 +7,22 @@
 @section('main')
   <div id="tab1" class="col s12">
       <div class="row">
-        @for ($i=0; $i < 3; $i++)
+        @for ($i=0; $i < 9; $i++)
         <div id="tab1" class="col s4">
             <div class="card">
               <div class="card-image">
                 <img src="images/sample-1.jpg">
-                <span class="card-title">記事タイトル</span>
+                <a href="/top"></a>
               </div>
               <div class="card-content">
-                <p>ここに記事の内容の一部を表示</p>
-              </div>
-              <div class="card-action">
-                <a href="#">This is a link</a>
+                <p>記事タイトル</p>
               </div>
             </div>
         </div>
         @endfor
       </div>
 
-      <div class="row">
+      {{-- <div class="row">
         @for ($i=0; $i < 2; $i++)
         <div id="tab1" class="col s6">
             <div class="card">
@@ -42,47 +39,7 @@
             </div>
         </div>
         @endfor
-      </div>
-
-      <div class="row">
-        @for ($i=0; $i < 3; $i++)
-        <div id="tab1" class="col s4">
-          <div class="card">
-            <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="images/sample-2.jpg">
-            </div>
-            <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">記事タイトル<i class="material-icons right">more_vert</i></span>
-              <p><a href="#">This is a link</a></p>
-            </div>
-            <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">記事タイトル<i class="material-icons right">close</i></span>
-              <p>ここに記事の内容の一部を表示</p>
-            </div>
-          </div>
-        </div>
-        @endfor
-     </div>
-
-     <div class="row">
-       @for ($i=0; $i < 2; $i++)
-       <div id="tab1" class="col s6">
-         <div class="card">
-           <div class="card-image waves-effect waves-block waves-light">
-             <img class="activator" src="images/sample-2.jpg">
-           </div>
-           <div class="card-content">
-             <span class="card-title activator grey-text text-darken-4">記事タイトル<i class="material-icons right">more_vert</i></span>
-             <p><a href="#">This is a link</a></p>
-           </div>
-           <div class="card-reveal">
-             <span class="card-title grey-text text-darken-4">記事タイトル<i class="material-icons right">close</i></span>
-             <p>ここに記事の内容の一部を表示</p>
-           </div>
-         </div>
-       </div>
-       @endfor
-    </div>
+      </div> --}}
 
   <div id="tab2" class="col s12">
     ニュースを表示
