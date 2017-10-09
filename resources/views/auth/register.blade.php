41782d91a0095117e525f8e3f873b9d8cac270b1
arinko <B5069@oic.jp>
2017-10-10 05:56:36 +0900

    button追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -113,6 +113,11 @@
       <label for="introduction">自己紹介</label>
     </div>
 
+    <div class="row">
+      <div class="input-field col s6"><a class="waves-effect waves-light btn">戻る</a></div>
+      <div class="input-field col s6"><a class="waves-effect waves-light btn">確認</a></div>
+    </div>
+
   </form>
 </div>
 
