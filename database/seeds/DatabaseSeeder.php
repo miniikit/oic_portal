f7a737894c2db380c46fab78edbecf1c6576c266
Ikki Fukui <b5379@oic.jp>
2017-10-31 13:21:02 +0900

    seeder編集

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -47,11 +47,18 @@ class UsersSeeder extends Seeder
     public function run()
     {
         DB::table('users')->delete();
-
-        //test data
+        DB::table('users')->insert([
+            'email' => 'b9999@oic.jp',
+            'name' => '山田太郎',
+            'name_kana' => 'ヤマダタロウ',
+            'authority_id' => 1,
+            'course_id' => 1,
+            'profile_id' => 1
+        ]);
         /*
+        //test data
         User::create([
-            'email' => 'b5163@oic.jp',
+            'email' => 'b9999@oic.jp',
             'name' => '山田　太郎',
             'name_kana' => 'ヤマダ　タロウ'
         ]);
@@ -64,14 +71,10 @@ class AuthoritiesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('authorities_master')->delete();
-
-
-        //test data
-        /*
-        Authority::create([
-            'authority_name' => 'アクセス権'
+        DB::table('authorities_master')->insert([
+            'authority_name' => '〜〜権',
         ]);
-        */
+        //test data
     }
 }
 
@@ -80,14 +83,12 @@ class ProfilesTableSeeder extends Seeder
     public function run()
     {
         DB::table('profiles_table')->delete();
-
-        //test data
-        /*
-        profile::create([
+        DB::table('profiles_table')->insert([
             'profile_image' => '',
-            'profile_name' => 'テスト'
+            'profile_name' => 'ホゲ',
+            'course_id' => 1
         ]);
-        */
+        //test data
     }
 }
 
@@ -96,14 +97,12 @@ class CoursesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('courses_master')->delete();
-
-        //test data
-        /*
-        course::create([
-           'course_name' => '総合情報メディア学科',
-           'course_depth' => '2'
+        DB::table('courses_master')->insert([
+            'course_name' => '総合情報メディア',
+            'parent_course_id' => 1,
+            'course_depth' => 1
         ]);
-        */
+        //test data
     }
 }
 
@@ -112,15 +111,13 @@ class ArticlesTableSeeder extends Seeder
     public function run()
     {
         DB::table('articles_table')->delete();
-
-        //test data
-        /*
-        article::create([
-           'article_title' => '新発売',
-           'article_text' => '〜〜〜〜〜〜〜〜〜〜、〜〜〜〜。',
-           'article_image' => ''
+        DB::table('articles_table')->insert([
+            'article_title' => 'あああ',
+            'article_text' => 'あいうえおあいうえおあいうえお',
+            'article_image' => '',
+            'news_site_id' => 1
         ]);
-        */
+        //test data
     }
 }
 
@@ -129,7 +126,10 @@ class ArticlesLikesTableSeeder extends Seeder
     public function run()
     {
         DB::table('articles_likes_table')->delete();
-
+        DB::table('articles_likes_table')->insert([
+            'article_id' => 1,
+            'user_id' => 1
+        ]);
         //test data
     }
 }
@@ -139,13 +139,12 @@ class ArticlesCommentsTableSeeder extends Seeder
     public function run()
     {
         DB::table('articles_comments_table')->delete();
-
-        //test data
-        /*
-        article_coment::creste([
-           'article_comment_text' => '〜〜〜〜〜〜〜〜〜、〜〜〜〜〜〜〜〜。'
+        DB::table('articles_comments_table')->insert([
+            'article_id' => 1,
+            'user_id' => 1,
+            'article_comment_text' => 'あああああああ'
         ]);
-        */
+        //test data
     }
 }
 
@@ -154,13 +153,13 @@ class ReportsTableSeeder extends Seeder
     public function run()
     {
         DB::table('reports_table')->delete();
-
-        //test data
-        /*
-        report::create([
-           'report_contents' => '〜〜〜〜〜〜〜〜〜、〜〜〜〜〜〜〜〜。'
+        DB::table('reports_table')->insert([
+            'report_category_id' => 1,
+            'user_id' => 1,
+            'report_contents' => 'あああああいいいいいいうううううえお',
+            'report_deal_status_id' =>1
         ]);
-        */
+        //test data
     }
 }
 
@@ -169,13 +168,12 @@ class ReportsDealsTableSeeder extends Seeder
     public function run()
     {
         DB::table('reports_deals_table')->delete();
-
-        //test data
-        /*
-        report_deal::create([
-           'report_deal_comment' => '〜〜〜〜〜〜〜〜〜、〜〜〜〜〜〜〜〜。'
+        DB::table('reports_deals_table')->insert([
+            'report_id' => 1,
+            'user_id' => 1,
+            'report_deal_comment' => 'ああああいいいいうううええお'
         ]);
-        */
+        //test data
     }
 }
 
@@ -184,7 +182,10 @@ class ReportsCategoriesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('reports_categories_master')->delete();
-
+        DB::table('reports_categories_master')->insert([
+            'report_category_name' => 'ああああ',
+            'report_risk_id' => 1
+        ]);
         //test data
     }
 }
@@ -194,7 +195,10 @@ class FriendsTableSeeder extends Seeder
     public function run()
     {
         DB::table('friends_table')->delete();
-
+        DB::table('friends_table')->insert([
+            'user_id' => 1,
+            'user2_id' => 1
+        ]);
         //test data
     }
 }
@@ -204,11 +208,10 @@ class CommunitiesCategoriesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('communities_categories_master')->delete();
-
-        //test data
-        community_category::create([
-           'community_category_name' => 'スポーツ'
+        DB::table('communities_categories_master')->insert([
+            'community_category_name' => '趣味'
         ]);
+        //test data
     }
 }
 
@@ -217,12 +220,12 @@ class CommunitiesTableSeeder extends Seeder
     public function run()
     {
         DB::table('communities_table')->delete();
-
-        //test data
-        community::create([
-           'community_title' => 'コミュニティA',
-           'community_contents' => 'フットボール部です'
+        DB::table('communities_table')->insert([
+            'community_title' => '〜〜会',
+            'community_contents' => 'あああああ',
+            'authority_id' => 1
         ]);
+        //test data
     }
 }
 
@@ -231,9 +234,11 @@ class CommunitiesParticipantsTableSeeder extends Seeder
     public function run()
     {
         DB::table('communities_participants_table')->delete();
-
+        DB::table('communities_participants_table')->insert([
+            'community_id' => 1,
+            'user_id' => 1
+        ]);
         //test data
-
     }
 }
 
@@ -242,7 +247,11 @@ class CommunitiesCommentsTableSeeder extends Seeder
     public function run()
     {
         DB::table('communities_comments_table')->delete();
-
+        DB::table('communities_comments_table')->insert([
+            'community_id' => 1,
+            'user_id' => 1,
+            'community_comment_contents' => 'ああああいいいいうううう'
+        ]);
         //test data
     }
 }
@@ -252,16 +261,14 @@ class NewsSitesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('news_sites_master')->delete();
-
-        //test data
-        /*
-        news_site::creat([
-           'news_site_url' => 'http~~~~',
-           'news_site_tag_title' => '',
-           'news_site_tag_article' => '',
-           'news_site_tag_image' => ''
+        DB::table('news_sites_master')->insert([
+            'news_site_url' => '',
+            'news_site_tag_title' => 'ITメディア',
+            'news_site_tag_article' => 'ああああ',
+            'news_site_tag_image' => '',
+            'news_site_category_id' => 1
         ]);
-        */
+        //test data
     }
 }
 
@@ -269,8 +276,10 @@ class NewsSitesCategoriesMasterSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('news_categories_master')->delete();
-
+        DB::table('news_sites_categories_master')->delete();
+        DB::table('news_sites_categories_master')->insert([
+            'news_site_category_name' => 'ニュース'
+        ]);
         //test data
     }
 }
@@ -280,11 +289,14 @@ class EventsTableSeeder extends Seeder
     public function run()
     {
         DB::table('events_table')->delete();
-
-        //test data
-        event::create([
-           'event_title' => 'php勉強会'
+        DB::table('events_table')->insert([
+            'event_title' => '勉強会',
+            'event_text' => 'アアアアアアああああああ',
+            'event_start_date_time' => '',
+            'event_end_date_time' => '',
+            'event_marker_id' => 1
         ]);
+        //test data
     }
 }
 
@@ -293,7 +305,11 @@ class EventsParticipantsTableSeeder extends Seeder
     public function run()
     {
         DB::table('events_participants_table')->delete();
-
+        DB::table('events_participants_table')->insert([
+            'event_id' => 1,
+            'event_user_id' => 1,
+            'event_authority_id' => 1
+        ]);
         //test data
     }
 }
@@ -303,7 +319,9 @@ class ReportsRisksDealStatusMasterSeeder extends Seeder
     public function run()
     {
         DB::table('reports_risks_deal_status_master')->delete();
-
+        DB::table('reports_risks_deal_status_master')->insert([
+            'report_risk_deal_status_name' => '対処中'
+        ]);
         //test data
     }
 }
@@ -313,7 +331,10 @@ class ReportsRisksCategoriesMasterSeeder extends Seeder
     public function run()
     {
         DB::table('reports_risks_categories_master')->delete();
-
+        DB::table('reports_risks_categories_master')->insert([
+            'report_risk_category_name' => '危険度大',
+            'report_risk_num' => 1
+        ]);
         //test data
     }
 }
@@ -323,8 +344,9 @@ class EventsAuthoritiesTableSeeder extends Seeder
     public function run()
     {
         DB::table('events_authorities_table')->delete();
-
+        DB::table('events_authorities_table')->insert([
+            'event_authority_name' => '編集権'
+        ]);
         //test data
-
     }
 }
