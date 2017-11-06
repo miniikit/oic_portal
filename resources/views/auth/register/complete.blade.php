39f9ddc4d60fe8a77fc32d17afc9b5adfb318381
arinko <B5069@oic.jp>
2017-11-07 02:11:37 +0900

    registerの修正、complete画面の作成

---
+++ resources/views/auth/register/complete.blade.php
@@ -0,0 +1,17 @@
+@extends('template.master')
+
+@section('css')
+  <link rel="stylesheet" href="/css/auth/complete.css">
+@endsection
+
+@section('main')
+
+<div class="content">
+  <div class="center-align">
+    <p class="text">送信しました</p>
+  </div>
+</div>
+
+
+
+@endsection
