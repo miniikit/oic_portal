51cfbe6fc1c6ffdb4483c5b683edec7f53e670b9
miniikit <b5122@oic.jp>
2017-11-21 15:07:43 +0900

    DB::テーブル修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -166,6 +166,7 @@ class ArticlesTableSeeder extends Seeder
                 'article_title' => $articles[$i],
                 'article_text' => $articles[$i],
                 'article_image' => '',
+                'article_url' => 'http://www.XXXXXXX.co.jp/articles/0000000000001',
                 'news_site_id' => $i
             ]);
         }
