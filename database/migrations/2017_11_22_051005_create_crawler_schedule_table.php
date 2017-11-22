d8f4f72318c1c8b4f3f324ba983207e200059aa6
miniikit <b5122@oic.jp>
2017-11-22 15:56:42 +0900

    DB::クローラー関連テーブル追加、修正

---
+++ database/migrations/2017_11_22_051005_create_crawler_schedule_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrawlerScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crawler_schedule', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('crawl_start_time');   // Crawl開始時刻
+            $table->dateTime('crawl_endt_time');    // Crawl終了時刻
+            $table->integer('crawl_status_id'); // Crawl状態
+            $table->integer('added_articles_count');  // 追加個数
+            $table->integer('user_id'); // 担当者ID
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crawler_schedule');
+    }
+}
