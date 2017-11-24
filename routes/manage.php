e4eac649e4e79701b76c38faf34170510cd059ee
miniikit <b5122@oic.jp>
2017-11-24 12:54:42 +0900

    manager/route 追加

--- routes/manage.php
+++ routes/manage.php
@@ -23,87 +23,87 @@ Route::get('/manage/crawl/1000', 'Manage\CrawlScheduleController@show')->name('m
  * 管理者 ー ニュースサイト
  */
 // 一覧
-Route::get('/manage/site', 'Manage\SiteController@index');
+Route::get('/manage/site', 'Manage\SiteController@index')->name('manager_site_list');
 // 詳細
-Route::get('/manage/site/1000', 'Manage\SiteController@show');
+Route::get('/manage/site/1000', 'Manage\SiteController@show')->name('manager_site_detail');
 // 編集
-Route::get('/manage/site/1000/edit', 'Manage\SiteController@edit');
+Route::get('/manage/site/1000/edit', 'Manage\SiteController@edit')->name('manager_site_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_site_update');
 
 
 /**
  * 管理者 ー 記事カテゴリ
  */
 // 一覧
-Route::get('/manage/article/category', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/article/category', 'Manage\ArticlesCategoriesController@index')->name('manager_article_category_list');
 // 詳細
-Route::get('/manage/article/category/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/article/category/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_article_category_detail');
 // 編集
-Route::get('/manage/article/category/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/article/category/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_article_category_edit');
 // 更新
-Route::post('/manage/article/category/1000/update','Manage\ArticlesCategoriesController@update');
+Route::post('/manage/article/category/1000/update','Manage\ArticlesCategoriesController@update')->name('manager_article_category_update');
 
 
 /**
  * 管理者 ー 記事
  */
 // 一覧
-Route::get('/manage/article', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/article', 'Manage\ArticlesCategoriesController@index')->name('manager_article_list');
 // 詳細
-Route::get('/manage/article/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/article/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_article_detail');
 // 編集
-Route::get('/manage/article/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/article/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_article_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_article_update');
 
 
 /**
  * 管理者 ー ユーザ
  */
 // 一覧
-Route::get('/manage/user', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/user', 'Manage\ArticlesCategoriesController@index')->name('manager_user_list');
 // 詳細
-Route::get('/manage/user/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/user/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_user_detail');
 // 編集
-Route::get('/manage/user/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/user/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_user_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_user_update');
 
 /**
  * 管理者 ー 管理者
  */
 // 一覧
-Route::get('/manage/manager', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/manager', 'Manage\ArticlesCategoriesController@index')->name('manager_employee_list');
 // 詳細
-Route::get('/manage/manager/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/manager/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_employee_detail');
 // 編集
-Route::get('/manage/manager/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/manager/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_employee_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_employee_update');
 
 
 /**
  * 管理者 - コミュニティ
  */
 // 一覧
-Route::get('/manage/community', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/community', 'Manage\ArticlesCategoriesController@index')->name('manager_community_list');
 // 詳細
-Route::get('/manage/community/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/community/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_community_detail');
 // 編集
-Route::get('/manage/community/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/community/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_community_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_community_update');
 
 
 /**
  * 管理者 - イベント
  */
 // 一覧
-Route::get('/manage/event', 'Manage\ArticlesCategoriesController@index');
+Route::get('/manage/event', 'Manage\ArticlesCategoriesController@index')->name('manager_event_list');
 // 詳細
-Route::get('/manage/event/1000', 'Manage\ArticlesCategoriesController@show');
+Route::get('/manage/event/1000', 'Manage\ArticlesCategoriesController@show')->name('manager_event_detail');
 // 編集
-Route::get('/manage/event/1000/edit', 'Manage\ArticlesCategoriesController@edit');
+Route::get('/manage/event/1000/edit', 'Manage\ArticlesCategoriesController@edit')->name('manager_event_edit');
 // 更新
-Route::post('/manage/site/1000/update','Manage\SiteController@update');
+Route::post('/manage/site/1000/update','Manage\SiteController@update')->name('manager_event_update');
