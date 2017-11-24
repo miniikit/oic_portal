397e4da14dfa913308b0395377e87f1cf2a8ae22
miniikit <b5122@oic.jp>
2017-11-24 11:22:22 +0900

    merge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Jobs\CheckNewArticles;
 
 class HomeController extends Controller
 {
     // ホーム（記事一覧）
     public function index()
     {
+        //$hoge = new CheckNewArticles();
+
+        //$this->dispatch(new CheckNewArticles);
         return view('home.list');
     }
 }
