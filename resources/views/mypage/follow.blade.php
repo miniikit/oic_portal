961a1b19a3814e00e86597a44abee351c39ebcfb
arinko <B5069@oic.jp>
2017-12-12 07:55:35 +0900

    follow画面修正

--- resources/views/mypage/follow.blade.php
+++ resources/views/mypage/follow.blade.php
@@ -6,20 +6,21 @@
 
 @section('main')
   <div class="row">
-    <div class="col s12 center-align">
+    <div class="page_title center-align">
       <h1 class="title">フォロー</h1>
     </div>
 
-    <div class="row">
+
+    <div class="content">
       @for ($i=1; $i < 10; $i++)
-        <div class="user_card col s4">
-          <div class="card">
+        <div class="user_card">
+          <div class="change card horizontal">
               <div class="card-image">
                 <img src="/images/profile_images/default.jpg" class="user-image circle">
               </div>
               <div class="card-stacked">
                 <div class="border-top card-content">
-                  <a class="link_color" href="{{ route('user_profile') }}"><h1 class="card-name">ユーザー名</h1></a>
+                  <a class="link_color" href=""><h1 class="card-name">ユーザー名</h1></a>
                 </div>
                 <div class="card-content">
                   <div class="user_info">
@@ -28,7 +29,7 @@
                       <span>10</span>
                     </div>
                     <div class="user_stats">
-                      <strong>follow</strong>
+                      <strong class="sp">follow</strong>
                       <span>50</span>
                     </div>
                     <div class="user_stats">
@@ -46,5 +47,20 @@
 @endsection
 
 @section('script')
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+
+  $(window).ready(function(){
+    var w = $(window).width();
+    var x = 601;
+    if (w <= x) {
+        $('.change').addClass("horizontal");
+    } else {
+        $('.change').removeClass("horizontal");
+    }
+  });
+</script>
 
 @endsection
