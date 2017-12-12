8df08b3ea35170c2aed3b5f1e0002a87a5346f5c
masakisaaan <masakilex@gmail.com>
2017-12-12 13:49:41 +0900

    update

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Community;
 
 class CommunityController extends Controller
 {
@@ -11,13 +12,30 @@ class CommunityController extends Controller
       return view('community.list');
     }
 
-    public function show()
+    public function show($id)
     {
-      return view('community.detail');
+      $community = app(Community::class)->find($id);
+      return view('community.detail',compact('community'));
     }
 
+
+    //作成
     public function make()
     {
-      return view('community.creat');
+      return view('community.create');
+    }
+
+    //完了
+    public function complete(Request $request)
+    {
+        $data = $request->all();
+        $community_model = app(Community::class);
+        $community_model->create([
+            'community_title' => $data['community_title'],
+            'community_contents' => $data['community_contents'],
+            'authority_id' => $data['article_text'],
+        ]);
+
+        return view('articles.complete');
     }
 }
