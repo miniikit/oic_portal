87a183e74e4e67f401c40d3a1e7d3c21611ac3b7
miniikit <b5122@oic.jp>
2017-12-20 13:57:39 +0900

    RouteServiceProvider::route->idに制約

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -9,20 +9,22 @@ class CommunityController extends Controller
 {
     public function index()
     {
-      return view('community.list');
+        return view('community.list');
     }
 
     public function show($id)
     {
-      $community = app(Community::class)->find($id);
-      return view('community.detail',compact('community'));
+        dd(12333);
+        $community = app(Community::class)->find($id);
+        return view('community.detail', compact('community'));
     }
 
 
     //作成
     public function make()
     {
-      return view('community.creat');
+        dd(123);
+        return view('community.create');
     }
 
     //完了
@@ -30,11 +32,7 @@ class CommunityController extends Controller
     {
         $data = $request->all();
         $community_model = app(Community::class);
-        $community_model->create([
-            'community_title' => $data['community_title'],
-            'community_contents' => $data['community_contents'],
-            'authority_id' => $data['article_text'],
-        ]);
+        $community_model->create(['community_title' => $data['community_title'], 'community_contents' => $data['community_contents'], 'authority_id' => $data['article_text'],]);
 
         return view('articles.complete');
     }
