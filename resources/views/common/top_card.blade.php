8d601ab7b7a3b39872215e183d08a8dbe08982ab
miniikit <b5122@oic.jp>
2018-01-15 01:21:41 +0900

    バグ修正

--- resources/views/common/top_card.blade.php
+++ resources/views/common/top_card.blade.php
@@ -22,10 +22,11 @@
             {{ csrf_field() }}
             <select required="required" name="category" onchange="submit()">
                 <option value="" disabled>カテゴリ</option>
-            @foreach($categories as $category)
+                @foreach($categories as $category)
                     @if(isset($categoryId))
                         @if($categoryId === $category->id)
-                            <option value="{{ $category->id }}" selected> {{ $category->articles_category_name }} </option>
+                            <option value="{{ $category->id }}"
+                                    selected> {{ $category->articles_category_name }} </option>
                         @else
                             <option value="{{ $category->id }}"> {{ $category->articles_category_name }} </option>
                         @endif
@@ -44,7 +45,8 @@
                     <div class="card-wrapper">
                         <div class="card-image">
                             <a href="{{ $article->article_url }}"><img src="{{ $article->article_image }}"></a>
-{{--  ここにタグの実装  --}}   <span class="ct-tag chip btn-floating halfway-fab circle blue-grey lighten-5">IT</span>
+                            {{--  ここにタグの実装  --}} <span
+                                    class="ct-tag chip btn-floating halfway-fab circle blue-grey lighten-5">IT</span>
                         </div>
                         <div class="card-stacked">
                             <a href="{{ $article->article_url }}"></a>
@@ -52,15 +54,15 @@
                                 <span class="card-title">{{ $article->article_title }}</span>
                                 <p class="card-text">{{ mb_strimwidth($article->article_text,0,132,"...") }}</p>
                             </div>
-                            {{-- <div class="card-action">
+                            <div class="card-action">
                                 <div class="tags">
                                     {{--@if($article->news_site_category_name)--}}
-                                        {{--<div class="chip">--}}
-                                            {{--{{ $article->news_site_category_name }}--}}
-                                        {{--</div>--}}
+                                    {{--<div class="chip">--}}
+                                    {{--{{ $article->news_site_category_name }}--}}
+                                    {{--</div>--}}
                                     {{--@endif--}}
                                 </div>
-                            </div> --}}
+                            </div>
                         </div>
                     </div>
                 </div>
