9f3fe69da6f154a61f7d7b5aac7c56522c75fb65
miniikit <b5122@oic.jp>
2018-02-07 14:04:04 +0900

    通知を仮実装

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -31,28 +31,28 @@
 
             @if(!Auth::guest())
                 <div class="icon-button col s12 right-align">
-                  <div class="col s12">
-                    <div class="us-box">
-                      <h1 class="user-name">投稿者：<a href="">投稿者名</a></h1>
+                    <div class="col s12">
+                        <div class="us-box">
+                            <h1 class="user-name">投稿者：<a href="">投稿者名</a></h1>
+                        </div>
+                        <div class="category-tag tags">
+                            <div class="chip">
+                                ユーザ
+                            </div>
+                            {{-- <div class="chip">
+                                IT
+                            </div>
+                            <div class="chip">
+                                デザイン
+                            </div>
+                            <div class="chip">
+                              映像
+                            </div>
+                            <div class="chip">
+                              ゲーム
+                            </div> --}}
+                        </div>
                     </div>
-                    <div class="category-tag tags">
-                        <div class="chip">
-                            ユーザ
-                        </div>
-                        {{-- <div class="chip">
-                            IT
-                        </div>
-                        <div class="chip">
-                            デザイン
-                        </div>
-                        <div class="chip">
-                          映像
-                        </div>
-                        <div class="chip">
-                          ゲーム
-                        </div> --}}
-                    </div>
-                  </div>
                     @if($active_fav == null)
                         <a class="fav-btn waves-effect" href="{{ $article->article_url.'/favorite' }}">
                             <i class="goodicon material-icons">favorite</i>
@@ -87,7 +87,9 @@
                                 <input type="text" id="icon_prefix2" class="materialize-textarea" name="comment_text"
                                        size="50">
                                 <div class="wap-comment">
-                                    <button type="submit" class="comment-submit waves-effect waves-light btn" required>投稿</button>
+                                    <button type="submit" class="comment-submit waves-effect waves-light btn" required>
+                                        投稿
+                                    </button>
                                 </div>
                                 {!! csrf_field() !!}
                             </form>
@@ -98,19 +100,19 @@
 
 
             @if(count($comments) > 0)
-            <div class="comment-area col s12">
-                <ul class="collection">
-                    @foreach($comments as $comment)
-                        <li class="collection-item avatar">
-                            <img class="circle" src="{{ $comment->profile_image }}">
-                            <span class="name">{{ $comment->profile_name }}</span>
-                            <p>
-                                {{ $comment->article_comment_text }}
-                            </p>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="comment-area col s12">
+                    <ul class="collection">
+                        @foreach($comments as $comment)
+                            <li class="collection-item avatar">
+                                <img class="circle" src="{{ $comment->profile_image }}">
+                                <span class="name">{{ $comment->profile_name }}</span>
+                                <p>
+                                    {{ $comment->article_comment_text }}
+                                </p>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
 
 
@@ -122,18 +124,19 @@
                     <h2>関連記事</h2>
                 </div>
                 @foreach($relatedArticles as $relatedArticle)
-                  <div class="col s12">
-                    <div class="card hoverable">
-                      <div class="card-wrapper">
-                        <div class="card-images">
-                            <a href="{{ route('user_article_detail',$relatedArticle->id) }}"><img src="{{ $relatedArticle->article_image }}"></a>
+                    <div class="col s12">
+                        <div class="card hoverable">
+                            <div class="card-wrapper">
+                                <div class="card-images">
+                                    <a href="{{ route('user_article_detail',$relatedArticle->id) }}"><img
+                                                src="{{ $relatedArticle->article_image }}"></a>
+                                </div>
+                                <div class="card-contents">
+                                    <p class="side-contents-text">{{ $relatedArticle->article_title }}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-contents">
-                            <p class="side-contents-text">{{ $relatedArticle->article_title }}</p>
-                        </div>
-                      </div>
                     </div>
-                  </div>
                 @endforeach
             </div>
         </div>
@@ -144,16 +147,16 @@
                     <h2>ランキング</h2>
                 </div>
                 @for ($i=0; $i < 3; $i++)
-                  <div class="col s12">
-                    <div class="relation card hoverable">
-                        <div class="card-images">
-                            <a href="details"><img src="/images/sample-3.jpg"></a>
-                        </div>
-                        <div class="card-contents">
-                            <p class="side-contents-text">ミニスーファミにやりたいゲームが入ってない！って方に、同じくらいの値段で買えるSFC用のレトロフリーク</p>
+                    <div class="col s12">
+                        <div class="relation card hoverable">
+                            <div class="card-images">
+                                <a href="details"><img src="/images/sample-3.jpg"></a>
+                            </div>
+                            <div class="card-contents">
+                                <p class="side-contents-text">ミニスーファミにやりたいゲームが入ってない！って方に、同じくらいの値段で買えるSFC用のレトロフリーク</p>
+                            </div>
                         </div>
                     </div>
-                  </div>
                 @endfor
             </div>
         </div>
--- resources/views/template/manage.blade.php
+++ resources/views/template/manage.blade.php
@@ -17,50 +17,54 @@
 
 <body>
 <header>
-  <div class="navbar-fixed">
-      <nav>
-        <div class="nav-wrapper">
-         <a href="{{ route('manager_home') }}" class="brand-logo">OIC-Portal</a>
-         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-         <ul id="nav-mobile" class="right hide-on-med-and-down">
-           <li><a class="links" href="{{ route('manager_home') }}">ホーム</a></li>
-           <li><a class="links" href="{{ route('manager_crawl_home') }}">クローラー管理</a></li>
-           <li><a class="links" href="{{ route('manager_article_list') }}">記事管理</a></li>
-           <li><a class="links" href="{{ route('manager_site_list') }}">サイト管理</a></li>
-           <li><a class="links" href="{{ route('manager_user_list') }}">ユーザ管理</a></li>
-           <li><a class="links" href="{{ route('manager_event_list') }}">イベント管理</a></li>
-           <li><a class="links" href="{{ route('manager_community_list') }}">コミュニティ管理</a></li>
-         </ul>
-         <ul class="side-nav" id="mobile-demo">
-           <li><a class="links" href="{{ route('manager_home') }}">ホーム</a></li>
-           <li><a class="links" href="{{ route('manager_crawl_home') }}">クローラー管理</a></li>
-           <li><a class="links" href="{{ route('manager_article_list') }}">記事管理</a></li>
-           <li><a class="links" href="{{ route('manager_site_list') }}">サイト管理</a></li>
-           <li><a class="links" href="{{ route('manager_site_list') }}">ユーザ管理</a></li>
-           <li><a class="links" href="{{ route('manager_event_list') }}">イベント管理</a></li>
-           <li><a class="links" href="{{ route('manager_community_list') }}">コミュニティ管理</a></li>
-         </ul>
-       </div>
-       <div class="nav-wrapper">
-         <div class="bread-rap">
-          @yield('breadcrumb')
-        </div>
-       </div>
-      </nav>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper">
+                <a href="{{ route('manager_home') }}" class="brand-logo">OIC-Portal</a>
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a class="links" href="{{ route('manager_home') }}">ホーム</a></li>
+                    <li><a class="links" href="{{ route('manager_crawl_home') }}">クローラー管理</a></li>
+                    <li><a class="links" href="{{ route('manager_article_list') }}">記事管理</a></li>
+                    <li><a class="links" href="{{ route('manager_site_list') }}">サイト管理</a></li>
+                    <li><a class="links" href="{{ route('manager_user_list') }}">ユーザ管理</a></li>
+                    <li><a class="links" href="{{ route('manager_event_list') }}">イベント管理</a></li>
+                    <li><a class="links" href="{{ route('manager_community_list') }}">コミュニティ管理</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
+                    <li><a class="links" href="{{ route('manager_home') }}">ホーム</a></li>
+                    <li><a class="links" href="{{ route('manager_crawl_home') }}">クローラー管理</a></li>
+                    <li><a class="links" href="{{ route('manager_article_list') }}">記事管理</a></li>
+                    <li><a class="links" href="{{ route('manager_site_list') }}">サイト管理</a></li>
+                    <li><a class="links" href="{{ route('manager_site_list') }}">ユーザ管理</a></li>
+                    <li><a class="links" href="{{ route('manager_event_list') }}">イベント管理</a></li>
+                    <li><a class="links" href="{{ route('manager_community_list') }}">コミュニティ管理</a></li>
+                </ul>
+            </div>
+            <div class="nav-wrapper">
+                <div class="bread-rap">
+                    @yield('breadcrumb')
+                </div>
+            </div>
+        </nav>
     </div>
 </header>
 
-    <main id="main">
-      <div class="container">
-        <div class="col s12 center">
-          <div class="card-panel valign-wrapper red lighten-3 @yield('alert_status')">
-            <h1 class="white-text">ここに通知を表示</h1>
-          </div>
-        </div>
+<main id="main">
+    <div class="container">
+        @if(isset($alerts))
+            <div class="col s12 center">
+                @foreach($alerts as $alert)
+                    <div class="card-panel valign-wrapper red lighten-3 @yield('alert_status')">
+                        <h1 class="white-text">ここに通知を表示</h1>
+                    </div>
+                @endforeach
+            </div>
+        @endif
         @yield('main')
         <div class="push"></div>
-      </div>
-    </main>
+    </div>
+</main>
 
 <!--- Script -->
 <!--Import jQuery before materialize.js-->
